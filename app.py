293f80544b0f832404248746acf0ff10b6e32fe2
cmin <chong.m2007@gmail.com>
2026-03-12 01:44:33 -0700

    fix port

--- app.py
+++ app.py
@@ -97,5 +97,4 @@ def reset():
 import os
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
